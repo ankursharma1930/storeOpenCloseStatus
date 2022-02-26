93e0d4569ae11f0f373f5f4d0b3eae6351c28c60
ankursharma1930 <ankur.ank1230@gmail.com>
2022-02-26 18:20:07 +0530

    create module in app folder

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * See copying.txt for license detail
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Amage_Store',__DIR__);
